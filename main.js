130fe1d2c1a18626d8bd5b6dc19853272ad282c0
AL <akuniniakuntumbal1@gmail.com>
2024-09-15 13:56:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('Happy Birthday!').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
